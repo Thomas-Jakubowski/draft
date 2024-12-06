eec49065e295b031ac968f5d8b84cae69fe9363e
Thomas <jakubowski.thomas@hotmail.com>
2024-12-06 09:39:09 +0100

    [ADD] put the diplay champions in another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'displayChampions.dart'; // Importer le fichier DisplayChampions
 
 void main() {
   runApp(const MyApp());
@@ -14,85 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('API Fetch Example')),
-        body: const FetchData(),
+        body: const DisplayChampions()
       ),
-    );
-  }
-}
-
-class FetchData extends StatefulWidget {
-  const FetchData({super.key});
-
-  @override
-  _FetchDataState createState() => _FetchDataState();
-}
-
-class _FetchDataState extends State<FetchData> {
-  List<String> _championNames = [];
-  List<String> _imageUrl = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
-    final responseVersion = await http.get(Uri.parse('https://ddragon.leagueoflegends.com/api/versions.json'));
-    String version = jsonDecode(responseVersion.body)[0];
-    final responseLegends = await http.get(Uri.parse('https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json'));
-
-    if (responseLegends.statusCode == 200) {
-      var data = jsonDecode(responseLegends.body);
-      var championsData = data['data'];
-
-      List<String> championIds = [];
-      List<String> championNames = [];
-      List<String> imageUrl = [];
-
-      championsData.forEach((key, value) {
-        championIds.add(value['id']);
-        championNames.add(value['name']);
-        imageUrl.add('https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${value['image']['full']}');
-      });
-
-      setState(() {
-        _championNames = championNames;
-        _imageUrl = imageUrl;
-      });
-
-      print(championIds);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: _imageUrl.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.network(
-                _imageUrl[index],
-                fit: BoxFit.cover,
-              ),
-              Text(
-                _championNames[index],
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
